7df10fbb298376b44a107ce5d7d238453de91a9e
TheGuyOfDoom <thomwiggers@gmail.com>
2011-04-07 16:08:11 +0000

    Better buildfile for windows, now build x64 on systems capable. (ISSUE 5)

--- crtorrent/make.bat
+++ crtorrent/make.bat
@@ -1,9 +1,35 @@
 @echo off
-set PATH=%PATH%;C:\Windows\Microsoft.NET\Framework\v4.0.30319\
+IF EXIST %WINDIR%\Microsoft.NET\Framework64\v4.0.30319\csc.exe GOTO SETX64
 
+set PATH=%PATH%;%WINDIR%\Microsoft.NET\Framework\v4.0.30319\
+pause
 
+IF NOT EXIST %WINDIR%\Microsoft.NET\Framework\v4.0.30319\ goto NO_NET_FRAMEWORK
+del /F bin
+mkdir bin
+
+:Compile
 Echo building:
-csc /optimize /reference:system.dll /reference:system.core.dll /main:crtorrent.Program /target:exe /out:crtorrent.exe *.cs Bencode\*.cs
+Echo ---------
+csc /optimize /reference:system.dll /reference:system.core.dll /main:crtorrent.Program /target:exe /out:bin/crtorrent.exe *.cs Bencode\*.cs
+Echo ---------
+GOTO END
 
+:SETX64
+Echo "Selecting x64..."
+Echo.
+Echo.
+set PATH=%PATH%;C:\Windows\Microsoft.NET\Framework64\v4.0.30319\
+GOTO COMPILE
 
+:NO_NET_FRAMEWORK
+ECHO "NO .NET Framework Found or wrong version"
+ECHO "Check if .NET v4.0.30319 is installed and try again"
+Echo. 
+Echo.
+Goto END
+
+:END
+Echo.
+Echo.
 pause
